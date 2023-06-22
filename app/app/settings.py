487ca641a1ecba74a0e4e989aaa533fbffd6b1b2
Mert <100792959+vuralmert@users.noreply.github.com>
2023-06-22 22:48:30 +0300

    Edited celery settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -95,12 +95,12 @@ DATABASES = {
 
 # Celery Settings
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Europe/Istanbul'
-CELERY_RESULT_BACKEND = 'django-db'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 # Celery Beat Settings
 
