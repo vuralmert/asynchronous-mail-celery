4ff7e8a51445f05e23f6a48905d436a2e58f1779
Mert <100792959+vuralmert@users.noreply.github.com>
2023-06-22 19:15:42 +0300

    New task created to send scheduled emails

--- app/send_mail/tasks.py
+++ app/send_mail/tasks.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from celery import shared_task
 from django.core.mail import send_mail
 from app.app import settings
+from datetime import datetime
 
 
 @shared_task(bind=True)
@@ -19,3 +20,23 @@ def send_mail_func(self):
             fail_silently=True,
         )
     return "Test mail sent"
+
+
+@shared_task()
+def schedule_mail_func():
+    users = get_user_model().objects.all()
+    current_time = datetime.now().strftime("%H:%M")
+
+    for user in users:
+        mail_subject = "Schedule Testing"
+        message = f"Current time: {current_time}"
+        to_email = user.email
+        send_mail(
+            subject=mail_subject,
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[to_email],
+            fail_silently=True,
+        )
+
+    return "Scheduled mail sent"
