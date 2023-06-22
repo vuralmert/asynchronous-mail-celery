4b10b5d2dbfc3d02aa77fdecf96e9a755f94f532
Mert <100792959+vuralmert@users.noreply.github.com>
2023-06-22 23:33:27 +0300

    Created new URL-Paths for functions

---
+++ app/send_mail/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('send_mail/', views.send_mail_to_all, name="send_mail"),
+    path('schedule_mail/', views.schedule_mail, name="schedule_mail"),
+]
--- app/send_mail/views.py
+++ app/send_mail/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
-from .tasks import send_mail_func
+from .tasks import send_mail_func, schedule_mail_func
 
 
 def send_mail_to_all(request):
     send_mail_func.delay()
     return HttpResponse("Test mail sent")
+
+
+def schedule_mail(request):
+    schedule_mail_func.delay()
+    return HttpResponse("Mail scheduled")
