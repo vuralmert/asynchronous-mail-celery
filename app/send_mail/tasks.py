b30064ca8934ba8488384576c4264329d476f3f6
Mert <100792959+vuralmert@users.noreply.github.com>
2023-06-22 21:32:55 +0300

    Added CC feature to scheduled mail

--- app/send_mail/tasks.py
+++ app/send_mail/tasks.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from celery import shared_task
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from app.app import settings
 from datetime import datetime
 
@@ -31,12 +31,15 @@ def schedule_mail_func():
         mail_subject = "Schedule Testing"
         message = f"Current time: {current_time}"
         to_email = user.email
-        send_mail(
+        cc_email = ['vural.mvce@gmail.com']
+
+        email = EmailMessage(
             subject=mail_subject,
-            message=message,
+            body=message,
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[to_email],
-            fail_silently=True,
+            to=[to_email],
+            cc=cc_email,
         )
+        email.send(fail_silently=True)
 
     return "Scheduled mail sent"
