df1d6881c6fe17b419eb4752dfe4511b31e6cbac
Mert <100792959+vuralmert@users.noreply.github.com>
2023-07-26 16:28:03 +0300

    Created URLs for mailing, storing and user interface

--- app/send_mail/urls.py
+++ app/send_mail/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import SendMailView, MailFormView, SentMailListView
 
 urlpatterns = [
-    path('send_mail/', views.send_mail_to_all, name="send_mail"),
+    path('send_mail/', SendMailView.as_view(), name="send_mail"),
+    path('mail_form/', MailFormView.as_view(), name="mail_form"),
+    path('sent_mails/', SentMailListView.as_view(), name='sent_mails'),
     path('schedule_mail/', views.schedule_mail, name="schedule_mail"),
 ]
