c6f58981839d80014ec38edf903953ca0690af60
Mert <100792959+vuralmert@users.noreply.github.com>
2023-07-26 16:26:48 +0300

    Necessary adjustments to display the newly added features on the admin page

--- app/send_mail/admin.py
+++ app/send_mail/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import StoredEmail
+from .models import SentMail, Media, StoredEmail
+
+
+class SentMailAdmin(admin.ModelAdmin):
+    list_display = ('to', 'subject', 'sent_at')
+    list_filter = ('sent_at',)
+    search_fields = ('to', 'subject')
+
+
+admin.site.register(SentMail, SentMailAdmin)
+
+
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('filename', 'uploaded_at')
+    list_filter = ('uploaded_at',)
+
+
+admin.site.register(Media, MediaAdmin)
 
 
 class StoredEmailAdmin(admin.ModelAdmin):
